20854d4bbb73e2604d2a255f92a208e79f620cf1
Ubuntu <ubuntu@ip-172-31-43-129.us-west-2.compute.internal>
2017-02-20 22:55:43 +0000

    Cats and dogs

--- my_notebooks/train_dog_cat_vgg.py
+++ my_notebooks/train_dog_cat_vgg.py
@@ -1,13 +1,14 @@
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential, load_model
+from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Flatten, Dense, Dropout, Lambda
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.preprocessing import image
 from keras.optimizers import Adam
+from keras.utils.np_utils import to_categorical
+
 
 DATA_PATH = '/data/dogscats/'
-INITIAL_WEIGHTS_FILE = '/data/trained_models/untrained_dog_cat_vgg.h5'
-EVENTUAL_WEIGHTS_FILE = '/data/trained_models/trained_dog_cat_vgg_1.h5'
 
 vgg_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32).reshape((1,1,3))
 def vgg_preprocess(x):
@@ -19,76 +20,66 @@ def ConvBlock(model, layers, filters):
         model.add(ZeroPadding2D((1, 1)))
         model.add(Convolution2D(filters, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
+    
 def FCBlock(model):
     model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    
+def BuildVGG():
+    model = Sequential()
+    model.add(Lambda(vgg_preprocess, input_shape=(224,224,3)))
+    ConvBlock(model, 2, 64)
+    ConvBlock(model, 2, 128)
+    ConvBlock(model, 3, 256)
+    ConvBlock(model, 3, 512)
+    ConvBlock(model, 3, 512)
 
-def train_model(trained_model, epoch = 1):
-    valid_batches = image.ImageDataGenerator().flow_from_directory(
-        DATA_PATH +'valid',
-        target_size=(224, 224),
-        class_mode='binary',
-        shuffle=False,
-        batch_size=16
-    )
-    train_batches = image.ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        shear_range=0.05,
-        zoom_range=0.1,
-        horizontal_flip=True,
-        dim_ordering='tf'
-    ).flow_from_directory(
-        DATA_PATH+'train',
-        target_size=(224, 224),
-        class_mode='binary',
-        shuffle=True,
-        batch_size=32
-    )
-    trained_model.fit_generator(train_batches,
-                        samples_per_epoch=train_batches.nb_sample,
-                        nb_epoch=epoch,
+    model.add(Flatten())
+    FCBlock(model)
+    FCBlock(model)
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(optimizer=Adam(lr=0.0000001),loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+model = BuildVGG()
+model.load_weights('/data/trained_models/dog_cat_complete_retrain_model_v2.5.h5')
+
+valid_batches = image.ImageDataGenerator().flow_from_directory(
+    DATA_PATH +'valid',
+    target_size=(224, 224),
+    class_mode='categorical',
+    shuffle=True,
+    batch_size=16
+)
+
+train_batches = image.ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.05,
+    zoom_range=0.1,
+    horizontal_flip=True,
+    dim_ordering='tf'
+).flow_from_directory(
+    DATA_PATH+'train',
+    target_size=(224, 224),
+    class_mode='categorical',
+    shuffle=True,
+    batch_size=32
+)
+
+i = 7
+while True:
+    print("v2."+str(i))
+    model.fit_generator(train_batches,
+                        samples_per_epoch=3200,
+                        nb_epoch=1,
                         validation_data=valid_batches,
                         nb_val_samples=valid_batches.nb_sample
     )
 
-
-def build_model():
-    seq_model = Sequential()
-    seq_model.add(Lambda(vgg_preprocess, input_shape=(224, 224, 3)))
-
-    ConvBlock(seq_model, 2, 64)
-    ConvBlock(seq_model, 2, 128)
-    ConvBlock(seq_model, 3, 256)
-    ConvBlock(seq_model, 3, 512)
-    ConvBlock(seq_model, 3, 512)
-
-    seq_model.add(Flatten())
-
-    FCBlock(seq_model)
-    FCBlock(seq_model)
-
-    seq_model.add(Dense(1, activation='sigmoid'))
-
-    seq_model.load_weights(INITIAL_WEIGHTS_FILE)
-
-    seq_model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
-
-    return seq_model
-
-
-model = build_model()
-
-layers = model.layers
-for layer in layers: layer.trainable=False
-layers[-1].trainable=True
-
-# layers = model.layers
-# first_dense_idx = [index for index,layer in enumerate(layers) if type(layer) is Dense][0]
-# for layer in layers[first_dense_idx:]: layer.trainable=True
-
-train_model(model, 1)
-
-model.save(EVENTUAL_WEIGHTS_FILE)
+    model.save_weights('/data/trained_models/dog_cat_complete_retrain_model_v2.'+str(i)+'.h5')
+    i += 1
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd docker
+make notebook DATA=~/work/data
